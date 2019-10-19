59599b84f740a2b86f8cdb4b5badd7439fbe9504
n-sai <nazera_93@mail.ru>
2019-10-19 15:22:23 +0600

    R1.1

--- raid1a.go
+++ raid1a.go
@@ -7,9 +7,10 @@ func Raid1a(width, height int) {
 		if i == 0 || i == height-1 {
 			for j := 0; j < width; j++ {
 				if width < 0 || height < 0 {
+					z01.PrintRune('\n')
 					break
 				} else if j == 0 || j == width-1 {
-					z01.PrintRune('0')
+					z01.PrintRune(48)
 				} else {
 					z01.PrintRune('-')
 				}
